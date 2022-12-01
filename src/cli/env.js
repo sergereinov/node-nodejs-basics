fdbfcc1a4decbeb8e26ff45d8332c66623649e25
sergereinov <47950629+sergereinov@users.noreply.github.com>
2022-12-01 19:26:49 +0300

    feat: implement cli/env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,18 @@
+/*
+    Task:
+        implement function that parses environment variables with prefix 'RSS_' and prints them
+        to the console in the format 'RSS_name1=value1; RSS_name2=value2'
+*/
+
+import { env } from 'node:process';
+
 const parseEnv = () => {
-    // Write your code here 
+    const rss = Object.entries(env)
+        .filter(([k]) => k.startsWith('RSS_'))
+        .map(([k, v]) => `${k}=${v}`)
+        .join('; ')
+
+    console.log(rss);
 };
 
 parseEnv();
