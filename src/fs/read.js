5c11678fb1faddde4035f204caee3dcb21cb55d4
sergereinov <47950629+sergereinov@users.noreply.github.com>
2022-12-01 17:45:41 +0300

    feat: implement fs/read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
+/*
+    Task:
+        implement function that prints content of the 'fileToRead.txt' into console
+        (if there's no file 'fileToRead.txt' Error with message 'FS operation failed' must be thrown)
+*/
+
+import { readFile } from 'node:fs/promises';
+
 const read = async () => {
-    // Write your code here 
+    readFile(new URL('./files/fileToRead.txt', import.meta.url), 'utf8')
+        .then(console.log)
+        .catch(() => {
+            throw new Error("FS operation failed");
+        })
 };
 
 await read();
