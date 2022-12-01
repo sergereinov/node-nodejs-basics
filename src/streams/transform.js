185704ffdfbe2df556b4bd5a08f77f28250b9f84
sergereinov <47950629+sergereinov@users.noreply.github.com>
2022-12-02 01:56:49 +0300

    feat: implement streams/transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,20 @@
+/*
+    Task:
+        implement function that reads data from 'process.stdin',
+        reverses text using Transform Stream and then writes it into 'process.stdout'
+*/
+
+import { stdin, stdout } from 'node:process';
+import { Transform } from 'node:stream';
+
 const transform = async () => {
-    // Write your code here 
+    const reverse = new Transform({
+        transform(chunk, encoding, callback) {
+            callback(null, chunk.reverse());
+        },
+    });
+
+    stdin.pipe(reverse).pipe(stdout);
 };
 
 await transform();
