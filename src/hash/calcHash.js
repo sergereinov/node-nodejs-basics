caed7bc18913a4354d8f7b95ddc558f5c99c6638
sergereinov <47950629+sergereinov@users.noreply.github.com>
2022-12-01 23:43:13 +0300

    feat: implement hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+/*
+    Task:
+        implement function that calculates SHA256 hash for file 'fileToCalculateHashFor.txt'
+        and logs it into console as hex
+*/
+
+import { readFile } from 'node:fs/promises';
+import { createHash } from 'node:crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+    readFile(new URL('./files/fileToCalculateHashFor.txt', import.meta.url))
+        .then(data => {
+            const hex = createHash('sha256').update(data).digest('hex');
+            console.log(hex);
+        });
 };
 
 await calculateHash();
