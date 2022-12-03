3a6bcbb3bdb4595d7c5087e7fa69091351c09217
sergereinov <47950629+sergereinov@users.noreply.github.com>
2022-12-03 04:05:06 +0300

    feat: implement worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,28 @@
+import { Worker } from 'node:worker_threads';
+import { cpus } from 'node:os';
+
 const performCalculations = async () => {
-    // Write your code here
+    const workers = Array.from(
+        { length: cpus().length },
+        (_, i) => new Promise((resolve) => {
+            const worker = new Worker(new URL('./worker.js', import.meta.url), {
+                workerData: i + 10,
+            });
+            worker.on('message', msg => {
+                resolve({ id: worker.threadId, status: 'resolved', data: msg });
+            });
+            worker.on('error', () => {
+                resolve({ id: worker.threadId, status: 'error', data: null });
+            });
+        })
+    );
+
+    Promise.all(workers).then(results => {
+        results.sort((a, b) => a.id - b.id);
+        console.log(results.map(r => {
+            return { status: r.status, data: r.data, };
+        }));
+    });
 };
 
 await performCalculations();
